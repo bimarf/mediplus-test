a16f448f9a3a0d42bf4da1feb154f011a9096f5c
bimarf <bimarenggaxvi@gmail.com>
2024-09-03 23:18:18 +0700

    feat:add register

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -87,6 +87,13 @@ export default function Login({ status, canResetPassword }) {
                         </Link>
                     )}
 
+                    <Link
+                        href={route('register')}
+                        className="ml-4 underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Register
+                    </Link>
+
                     <PrimaryButton className="ml-4" disabled={processing}>
                         Log in
                     </PrimaryButton>
