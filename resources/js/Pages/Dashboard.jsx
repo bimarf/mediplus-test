6a1810cde7c1e47e6158d5415097d098b3d24413
bimarf <bimarenggaxvi@gmail.com>
2025-01-11 06:09:20 +0700

    trigger

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard(props) {
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard1</h2>}
         >
             <Head title="Dashboard" />
 
